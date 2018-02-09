375d8a391ef2556befc839b8005ef6acf3584537
Tilman Lindig <tilman.lindig@rogers.com>
2018-02-09 01:32:14 -0800

    testing hook on ledger

--- server/data/models/ledger.js
+++ server/data/models/ledger.js
@@ -1,40 +1,92 @@
 import Type from 'sequelize';
 import sequelize from '../sequelize';
+import Active from './active';
 
-const Ledger = sequelize.define('Ledger', {
-  id: {
-    primaryKey: true,
-    type: Type.INTEGER,
-    allowNull: false,
-  },
-  date: {
-    type: Type.DATE,
-
-  },
-  debit_user: {
-    primaryKey: true,
-    type: Type.STRING,
-    allowNull: false,
-    set(val) {
-      this.setDataValue('debit_user', val.toLowerCase());
+const Ledger = sequelize.define(
+  'Ledger',
+  {
+    id: {
+      primaryKey: true,
+      type: Type.INTEGER,
+      allowNull: false,
+    },
+    date: {
+      type: Type.DATE,
+    },
+    debit_user: {
+      primaryKey: true,
+      type: Type.STRING,
+      allowNull: false,
+      set(val) {
+        this.setDataValue('debit_user', val.toLowerCase());
+      },
+    },
+    credit_user: {
+      primaryKey: true,
+      type: Type.STRING,
+      allowNull: false,
+      set(val) {
+        this.setDataValue('credit_user', val.toLowerCase());
+      },
+    },
+    amount: {
+      type: Type.DOUBLE(10, 2),
+      allowNull: false,
     },
   },
-  credit_user: {
-    primaryKey: true,
-    type: Type.STRING,
-    allowNull: false,
-    set(val) {
-      this.setDataValue('credit_user', val.toLowerCase());
+  {
+    freezeTableName: true,
+    tableName: 'Ledger',
+    hooks: {
+      afterCreate(transaction, options) {
+        console.log('K');
+        Active.findOne({
+          where: {
+            ower: transaction.credit_user,
+            owed: transaction.debit_user,
+          },
+        }).then(record => {
+          if (record) {
+            record.updateAttributes({
+              amount: record.amount + transaction.amount,
+            });
+          } else {
+            Active.findOne({
+              where: {
+                ower: transaction.debit_user,
+                owed: transaction.credit_user,
+              },
+            }).then(rec => {
+              if (rec) {
+                if (rec.amount > transaction.amount) {
+                  rec
+                    .update({
+                      amount: rec.amount - transaction.amount,
+                    })
+                    .then(() => {});
+                } else if (rec.amount === transaction.amount) {
+                  rec.destroy({ force: true });
+                } else {
+                  Active.create({
+                    ower: transaction.credit_user,
+                    owed: transaction.debit_user,
+                    amount: transaction.amount - rec.amount,
+                  });
+                  rec.destroy({ force: true });
+                }
+              } else {
+                Active.create({
+                  ower: transaction.credit_user,
+                  owed: transaction.debit_user,
+                  amount: transaction.amount,
+                });
+              }
+            });
+          }
+        });
+      },
     },
   },
-  amount: {
-    type: Type.DOUBLE(10,2),
-    allowNull: false,
-  },
-},
-{
-  freezeTableName: true,
-  tableName: 'Ledger'
-});
+);
 
 export default Ledger;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,8 @@ import { Router } from 'express';
 import login from './login';
 import signup from './signup';
 import profile from './profile';
-import sequelize from  '../data/sequelize';
+import sequelize from '../data/sequelize';
+import { Ledger } from '../data/models/index';
 
 const router = Router();
 
@@ -12,33 +13,19 @@ router.use('/signup', signup);
 router.use('/profile', profile);
 
 router.get('/', (req, res) => {
-  sequelize.query(`
-  CREATE TRIGGER isActive
-     AFTER INSERT ON LEDGER
-     FOR EACH ROW
-     BEGIN
-       DECLARE x DOUBLE(10,2);
-       IF ((NEW.DEBIT_USER, NEW.CREDIT_USER) IN (
-         SELECT OWED, OWER FROM ACTIVE
-       )) THEN
-          UPDATE ACTIVE SET ACTIVE.AMOUNT = ACTIVE.AMOUNT+NEW.AMOUNT WHERE NEW.DEBIT_USER = OWED AND NEW.CREDIT_USER = OWER;
-       ELSEIF ((NEW.DEBIT_USER, NEW.CREDIT_USER) IN (
-         SELECT OWER, OWED FROM ACTIVE
-       )) THEN
-          SET x := (SELECT AMOUNT FROM ACTIVE WHERE NEW.DEBIT_USER = OWER AND NEW.CREDIT_USER = OWED);
-          IF(x > NEW.AMOUNT) THEN
-            UPDATE ACTIVE SET AMOUNT = AMOUNT-NEW.AMOUNT WHERE NEW.DEBIT_USER = OWER AND NEW.CREDIT_USER = OWED;
-          ELSEIF (NEW.AMOUNT = x) THEN
-            DELETE FROM ACTIVE WHERE NEW.DEBIT_USER = OWER AND NEW.CREDIT_USER = OWED;
-          ELSE
-            DELETE FROM ACTIVE WHERE NEW.DEBIT_USER = OWER AND NEW.CREDIT_USER = OWED;
-            INSERT INTO ACTIVE (OWER,OWED,AMOUNT) VALUES(NEW.CREDIT_USER, NEW.DEBIT_USER, NEW.AMOUNT - x);
-          END IF;
-       ELSE
-          INSERT INTO ACTIVE (OWER, OWED, AMOUNT) VALUES (NEW.CREDIT_USER, NEW.DEBIT_USER,NEW.AMOUNT);
-       END IF;
-    END;
-  `).then((res) => console.log(res));
+  sequelize
+    .sync({
+      force: false,
+    })
+    .then(() => {
+      Ledger.create({
+        id: 5,
+        date: '2018-01-01',
+        debit_user: 'bbb',
+        credit_user: 'aaa',
+        amount: 20,
+      });
+    });
   res.send('hello world');
 });
 
