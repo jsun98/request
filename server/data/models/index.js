eadac6bbed911702488f395216c2d26804a08cae
Tilman Lindig <tilman.lindig@rogers.com>
2018-02-11 23:59:47 -0800

    Fixed trigger, runs once on startup

--- server/data/models/index.js
+++ server/data/models/index.js
@@ -9,6 +9,39 @@ const sync = (...args) => {
     .sync(...args)
     .then(() => {
       console.info('Successfully synced database.');
+      sequelize.query('DROP TRIGGER IF EXISTS updateACTIVESs;').then(() => {
+        sequelize
+          .query(
+            `
+          CREATE TRIGGER updateACTIVESs
+             AFTER INSERT ON LEDGER
+             FOR EACH ROW
+             BEGIN
+               DECLARE x DOUBLE(10,2);
+               IF ((NEW.debitUser, NEW.creditUser) IN (
+                 SELECT OWED, OWER FROM ACTIVES
+               )) THEN
+                  UPDATE ACTIVES SET ACTIVES.AMOUNT = ACTIVES.AMOUNT+NEW.AMOUNT WHERE NEW.debitUser = OWED AND NEW.creditUser = OWER;
+               ELSEIF ((NEW.DEBITUSER, NEW.CREDITUSER) IN (
+                 SELECT OWER, OWED FROM ACTIVES
+               )) THEN
+                  SET x := (SELECT AMOUNT FROM ACTIVES WHERE NEW.debitUser = OWER AND NEW.creditUser = OWED);
+                  IF(x > NEW.AMOUNT) THEN
+                    UPDATE ACTIVES SET AMOUNT = AMOUNT-NEW.AMOUNT, updatedAt=CURRENT_TIMESTAMP WHERE NEW.debitUser = OWER AND NEW.creditUser = OWED;
+                  ELSEIF (NEW.AMOUNT = x) THEN
+                    DELETE FROM ACTIVES WHERE NEW.debitUser = OWER AND NEW.creditUser = OWED;
+                  ELSE
+                    DELETE FROM ACTIVES WHERE NEW.debitUser = OWER AND NEW.creditUser = OWED;
+                    INSERT INTO ACTIVES (OWER,OWED,AMOUNT, createdAt, updatedAt) VALUES(NEW.creditUser, NEW.debitUser, NEW.AMOUNT - x,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+                  END IF;
+               ELSE
+                  INSERT INTO ACTIVES (OWER, OWED, AMOUNT, createdAt, updatedAt) VALUES (NEW.creditUser, NEW.debitUser,NEW.AMOUNT,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+               END IF;
+            END;
+          `,
+          )
+          .then(() => console.log('Trigger created successfully'));
+      });
     })
     .catch(err => {
       console.error('Unable to sync database', err);
